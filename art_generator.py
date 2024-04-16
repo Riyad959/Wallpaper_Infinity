2834c22e5373b4a4c20f6f36b144a0c7ae8b95eb
Riyad959 <riyadul15-4828@diu.edu.bd>
2024-04-16 21:45:23 +0600

    add circle and polygon with all styles

--- art_generator.py
+++ art_generator.py
@@ -342,3 +342,363 @@ class Canvas:
         # empty
         if style == art_styles_list[6]:     
             pass
+        
+   
+   
+   
+   # circle all
+        
+    def generate_circles(self, complexity, cp, style, layer, magnitude, fill):
+        
+        
+         # chaotic
+        if style == art_styles_list[0]:    
+            for i in range(complexity):
+                rad = randint(magnitude[0], magnitude[1])
+                if fill == 1:
+                    fill_type = randint(5, rad//2)
+                else:
+                    fill_type = 0
+                centerX = randint(-25, self.width + 25)
+                centerY = randint(-25, self.height + 25)
+                current_color = cp[randint(0, len(cp) - 1)]
+                alpha_surface = pg.Surface((rad*2, rad*2))
+                alpha_surface.fill((0, 0, 0))
+                pg.draw.circle(alpha_surface, pg.Color(current_color), (rad, rad), rad, fill_type)
+                alpha_surface.set_colorkey((0, 0, 0))
+                alpha_surface.set_alpha(randint(150, 255))
+                layer.blit(alpha_surface, (centerX-rad, centerY-rad))
+
+        
+        
+        # horizontal
+        if style == art_styles_list[1]:     
+            row_circle_count = complexity // 2 + 2
+            point = self.width // (row_circle_count - 2)
+            row_count = self.height // point + 2
+            color_one = cp[randint(0, len(cp) - 1)]
+            color_two = cp[randint(0, len(cp) - 1)]
+            current_color = color_one
+            while color_two == color_one:
+                color_two = cp[randint(0, len(cp) - 1)]
+
+            for i in range(row_count):
+                if i % 2 == 0:
+                    current_color = color_one if current_color == color_two else color_two
+                for j in range(row_circle_count):
+                    posX = j * point
+                    posY = i * point
+                    rad = randint(magnitude[0], magnitude[1])
+                    if fill == 1:
+                        fill_type = randint(5, rad // 2)
+                    else:
+                        fill_type = 0
+                    if i % 2 == 0:
+                        alpha_surface = pg.Surface((rad * 2, rad * 2))
+                        alpha_surface.fill((0, 0, 0))
+                        pg.draw.circle(alpha_surface, pg.Color(current_color), (rad, rad), rad, fill_type)
+                        alpha_surface.set_colorkey((0, 0, 0))
+                        alpha_surface.set_alpha(randint(150, 255))
+                        layer.blit(alpha_surface, (posX - rad, posY - rad))
+
+        
+        
+        # vertical
+        if style == art_styles_list[2]:  
+            row_circle_count = complexity // 2 + 2
+            point = self.width // (row_circle_count - 2)
+            row_count = self.height // point + 2
+            color_one = cp[randint(0, len(cp) - 1)]
+            color_two = cp[randint(0, len(cp) - 1)]
+            current_color = color_one
+            while color_two == color_one:
+                color_two = cp[randint(0, len(cp) - 1)]
+
+            for i in range(row_count):
+                for j in range(row_circle_count):
+                    if (j+1) % 2 == 0:
+                        current_color = color_one if current_color == color_two else color_two
+                    posX = j * point
+                    posY = i * point
+                    rad = randint(magnitude[0], magnitude[1])
+                    if fill == 1:
+                        fill_type = randint(5, rad // 2)
+                    else:
+                        fill_type = 0
+                    if j % 2 == 0:
+                        alpha_surface = pg.Surface((rad * 2, rad * 2))
+                        alpha_surface.fill((0, 0, 0))
+                        pg.draw.circle(alpha_surface, pg.Color(current_color), (rad, rad), rad, fill_type)
+                        alpha_surface.set_colorkey((0, 0, 0))
+                        alpha_surface.set_alpha(randint(150, 255))
+                        layer.blit(alpha_surface, (posX - rad, posY - rad))
+
+        
+        
+        # mosaic
+        if style == art_styles_list[3]:     
+            row_circle_count = complexity
+            rad = self.width // row_circle_count
+            row_count = self.height // rad + 1
+            color_one = cp[randint(0, len(cp) - 1)]
+            color_two = cp[randint(0, len(cp) - 1)]
+            while color_two == color_one:
+                color_two = cp[randint(0, len(cp) - 1)]
+            for i in range(row_count):
+                for j in range(row_circle_count):
+                    if fill == 1:
+                        fill_type = rad//4
+                    else:
+                        fill_type = 0
+                    current_color = color_one if (i + j) % 2 == 0 else color_two
+                    posX = rad + j * (rad+1) * 2
+                    posY = rad + i * (rad+1) * 2
+                    pg.draw.circle(layer, pg.Color(current_color), (posX, posY), rad, fill_type)
+
+        
+        
+        # corner
+        if style == art_styles_list[4]:     
+            for i in range(complexity*2):
+                current_color = cp[randint(0, len(cp)-1)]
+                corner = randint(0, 3)
+                x_area, y_area = (0, 0), (0, 0)
+                if corner == 0:
+                    x_area, y_area = (-50, self.width//3), (-50, self.height//3)
+                if corner == 1:
+                    x_area, y_area = (self.width//1.5, self.width+50), (-50, self.height//3)
+                if corner == 2:
+                    x_area, y_area = (self.width//1.5, self.width+50), (self.height//1.5, self.height+50)
+                if corner == 3:
+                    x_area, y_area = (-50, self.width//3), (self.height//1.5, self.height+50)
+
+                posX = randint(x_area[0], x_area[1])
+                posY = randint(y_area[0], y_area[1])
+                rad = randint(magnitude[0], magnitude[1])
+
+                if fill == 1:
+                    fill_type = randint(5, rad//2)
+                else:
+                    fill_type = 0
+
+                alpha_surface = pg.Surface((rad * 2, rad * 2))
+                alpha_surface.fill((0, 0, 0))
+                pg.draw.circle(alpha_surface, pg.Color(current_color), (rad, rad), rad, fill_type)
+                alpha_surface.set_colorkey((0, 0, 0))
+                alpha_surface.set_alpha(randint(150, 255))
+                layer.blit(alpha_surface, (posX - rad, posY - rad))
+
+        
+        
+        # center
+        if style == art_styles_list[5]:     
+            in_x_area, in_y_area = (self.width//4, 3*self.width//4), (self.height//4, 3*self.height//4)
+            out_x_area, out_y_area = (self.width//6, 5*self.width//6), (self.height//6, 5*self.height//6)
+
+            for i in range(complexity):
+                if fill == 1:
+                    fill_type = randint(magnitude[0], magnitude[1])
+                else:
+                    fill_type = 0
+                random_number = randint(0, 5)
+                if random_number < 4:
+                    center_x = randint(in_x_area[0], in_x_area[1])
+                    center_y = randint(in_y_area[0], in_y_area[1])
+                else:
+                    center_x = randint(out_x_area[0], out_x_area[1])
+                    center_y = randint(out_y_area[0], out_y_area[1])
+
+                rad = randint(magnitude[0], magnitude[1])
+                current_color = cp[randint(0, len(cp)-1)]
+
+                if fill == 1:
+                    fill_type = randint(5, rad//2)
+                else:
+                    fill_type = 0
+
+                alpha_surface = pg.Surface((rad * 2, rad * 2))
+                alpha_surface.fill((0, 0, 0))
+                pg.draw.circle(alpha_surface, pg.Color(current_color), (rad, rad), rad, fill_type)
+                alpha_surface.set_colorkey((0, 0, 0))
+                alpha_surface.set_alpha(randint(150, 255))
+                layer.blit(alpha_surface, (center_x - rad, center_y - rad))
+
+        
+        
+        # empty
+        if style == art_styles_list[6]:    
+            pass
+        
+        
+    
+    # polygon all
+    
+    def generate_polygons(self, complexity, cp, style, layer, magnitude, fill):
+        
+        
+        # chaotic
+        if style == art_styles_list[0]:     
+            for i in range(complexity//2):
+                current_color = cp[randint(0, len(cp)-1)]
+                if fill == 0:
+                    fill_type = 0
+                else:
+                    fill_type = randint(magnitude[0]//5, magnitude[1]//5)
+                points_count = randint(3, 5)
+                points = []
+                first_point = [randint(0, self.width), randint(0, self.height)]
+                for _ in range(points_count):
+                    points.append([randint(first_point[0]-magnitude[1]*2, first_point[0]+magnitude[1]*2),
+                                   randint(first_point[1]-magnitude[1]*2, first_point[1]+magnitude[1]*2)])
+
+                pg.draw.polygon(layer, pg.Color(current_color), points, fill_type)
+
+        
+        
+        # horizontal
+        
+        if style == art_styles_list[1]:     
+            row_polygon_count = int(complexity // 2)
+            row_count = complexity // 3
+            x_interval = self.width // row_polygon_count
+            y_interval = self.height // row_count
+
+            color_one = cp[randint(0, len(cp) - 1)]
+            color_two = cp[randint(0, len(cp) - 1)]
+            while color_two == color_one:
+                color_two = cp[randint(0, len(cp) - 1)]
+            current_color = color_one
+            for i in range(row_count):
+                if (i + 1) % 2 == 0:
+                    current_color = color_one if current_color == color_two else color_two
+                for j in range(row_polygon_count):
+                    if fill == 0:
+                        fill_type = 0
+                    else:
+                        fill_type = randint(magnitude[0] // 10, magnitude[1] // 10)
+
+                    x_area = (x_interval * j, x_interval * (j + 1))
+                    y_area = (y_interval * i, y_interval * (i + 1))
+                    point_count = randint(3, 5)
+                    points = []
+                    if (i + 1) % 2 == 0:
+                        for k in range(point_count):
+                            points.append((randint(x_area[0], x_area[1]), randint(y_area[0], y_area[1])))
+                        pg.draw.polygon(layer, pg.Color(current_color), points, fill_type)
+
+        
+        
+        # vertical
+        if style == art_styles_list[2]:    
+            row_polygon_count = int(complexity // 2)
+            row_count = complexity // 3
+            x_interval = self.width // row_polygon_count
+            y_interval = self.height // row_count
+
+            color_one = cp[randint(0, len(cp) - 1)]
+            color_two = cp[randint(0, len(cp) - 1)]
+            while color_two == color_one:
+                color_two = cp[randint(0, len(cp) - 1)]
+            current_color = color_one
+            for i in range(row_count):
+                for j in range(row_polygon_count):
+                    if fill == 0:
+                        fill_type = 0
+                    else:
+                        fill_type = randint(magnitude[0] // 10, magnitude[1] // 10)
+
+                    x_area = (x_interval * j, x_interval * (j + 1))
+                    y_area = (y_interval * i, y_interval * (i + 1))
+                    point_count = randint(3, 5)
+                    points = []
+                    if j % 2 == 0:
+                        current_color = color_one if current_color == color_two else color_two
+                        for k in range(point_count):
+                            points.append((randint(x_area[0], x_area[1]), randint(y_area[0], y_area[1])))
+                        pg.draw.polygon(layer, pg.Color(current_color), points, fill_type)
+
+        
+        
+         # mosaic
+        if style == art_styles_list[3]:    
+            row_polygon_count = int(complexity // 2)
+            row_count = complexity // 3
+            x_interval = self.width // row_polygon_count
+            y_interval = self.height // row_count
+
+            color_one = cp[randint(0, len(cp) - 1)]
+            color_two = cp[randint(0, len(cp) - 1)]
+            while color_two == color_one:
+                color_two = cp[randint(0, len(cp) - 1)]
+            for i in range(row_count):
+                for j in range(row_polygon_count):
+                    if fill == 0:
+                        fill_type = 0
+                    else:
+                        fill_type = randint(magnitude[0] // 10, magnitude[1] // 10)
+
+                    current_color = color_one if (i + j) % 2 == 0 else color_two
+                    x_area = (x_interval*j, x_interval*(j+1))
+                    y_area = (y_interval*i, y_interval*(i+1))
+                    point_count = randint(3, 5)
+                    points = []
+                    for k in range(point_count):
+                        points.append((randint(x_area[0], x_area[1]), randint(y_area[0], y_area[1])))
+                    pg.draw.polygon(layer, pg.Color(current_color), points, fill_type)
+
+        
+        
+        # corner
+        if style == art_styles_list[4]:     
+            x_areas = [(-100, self.width//3), (2*self.width//3, self.width+100),
+                       (2*self.width//3, self.width+100), (-100, self.width//3)]
+
+            y_areas = [(-100, self.height//2-50), (-100, self.height//2-100),
+                       (self.height//2+100, self.height+100), (self.height//2+100, self.height+100)]
+
+            point_count = randint(3, 5)
+            for i in range(complexity//2):
+                corner = randint(0, 3)
+                if fill == 0:
+                    fill_type = 0
+                else:
+                    fill_type = randint(magnitude[0] // 10, magnitude[1] // 10)
+                current_color = cp[randint(0, len(cp)-1)]
+                points = []
+                for j in range(point_count):
+                    pos = (randint(x_areas[corner][0], x_areas[corner][1]),
+                           randint(y_areas[corner][0], y_areas[corner][1]))
+                    points.append(pos)
+
+                pg.draw.polygon(layer, pg.Color(current_color), points, fill_type)
+
+        
+        
+        # center
+        if style == art_styles_list[5]:     
+            x_inner_area = [self.width // 4, 3 * self.width // 4]
+            x_outer_area = [self.width // 6, 5 * self.width // 6]
+            y_area = [self.height // 4, 3 * self.height // 4]
+            for i in range(complexity // 4):
+                if fill == 0:
+                    fill_type = 0
+                else:
+                    fill_type = randint(magnitude[0] // 10, magnitude[1] // 10)
+
+                current_color = cp[randint(0, len(cp)-1)]
+
+                point_count = randint(3, 4)
+                points = []
+                for j in range(point_count):
+                    if randint(0, 6) <= 4:
+                        pos = (randint(x_inner_area[0], x_inner_area[1]), randint(y_area[0], y_area[1]))
+                    else:
+                        pos = (randint(x_outer_area[0], x_outer_area[1]), randint(y_area[0], y_area[1]))
+
+                    points.append(pos)
+                pg.draw.polygon(layer, pg.Color(current_color), points, fill_type)
+
+        
+        # mmpty
+        if style == art_styles_list[6]:     
+            pass
